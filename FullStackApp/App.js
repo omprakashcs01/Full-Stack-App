a2f7733df6de4cfd48bd15185e424947e48cd5eb
Om Prakash <omprakashcs01@gmail.com>
2024-06-04 21:40:57 +0530

    basic setup

---
+++ FullStackApp/App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Register from "./src/screens/auth/Register";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
